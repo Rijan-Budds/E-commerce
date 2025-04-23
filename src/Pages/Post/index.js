eb55183cd62f7d903e8f0e31e32ac7a9dce72788
Rijan-Budds <buddsrijan@gmail.com>
2025-04-23 04:23:24 +0000

    some changes

---
+++ src/Pages/Post/index.js
@@ -0,0 +1,12 @@
+export function Post() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-12">
+          <h1>Post</h1>
+          <p>This is the Post component.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
